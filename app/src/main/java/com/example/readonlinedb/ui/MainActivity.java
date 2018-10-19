f02ae3b058f75423f6b5232eefcd4b1914899d09
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-10-19 22:40:57 +0200

    add retrofit dependency instead of volley

--- app/src/main/java/com/example/readonlinedb/ui/MainActivity.java
+++ app/src/main/java/com/example/readonlinedb/ui/MainActivity.java
@@ -5,22 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.readonlinedb.R;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -59,8 +48,9 @@ public class MainActivity extends AppCompatActivity{
 
                 mUserEmail = mEmialEditTxt.getText().toString();
                 mUserPassword = mPasswordEditText.getText().toString();
+
                 if(!TextUtils.isEmpty(mUserEmail) && !TextUtils.isEmpty(mUserPassword)){
-                    login();
+
                 }else
                     Toast.makeText(MainActivity.this, "invalid emial or password", Toast.LENGTH_SHORT).show();
             }
@@ -68,31 +58,7 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    public void login(){
-        Log.d("mainactivity","login");
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,
-                LOGIN_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("mainactivity","response is "+response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("mainactivity","error is "+error.getMessage());
-            }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> map = new HashMap<>();
-                map.put("email",mUserEmail);
-                map.put("password",mUserPassword);
-                return map;
-            }
-        };
 
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-    }
+
 }
